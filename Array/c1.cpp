aebc45fa96b97aa5e2fb3f90af7e09c5777212fa
ronilkansoda <ronilkansoda73@gmail.com>
2024-10-18 20:49:40 +0530

    adding pointer's concept in c1.cpp

--- Array/c1.cpp
+++ Array/c1.cpp
@@ -5,46 +5,78 @@
 
 using namespace std;
 
-void ChangeArr(int arr[], int size)
-{
-    printf("in funciton\n");
-    for (int i = 0; i < size; i++)
-    {
-        arr[i] = arr[i] * 2;
-    }
-}
-typedef struct
-{
-    int index1;
-    int index2;
-} Indices;
+// void ChangeArr(int arr[], int size)
+// {
+//     printf("in funciton\n");
+//     for (int i = 0; i < size; i++)
+//     {
+//         arr[i] = arr[i] * 2;
+//     }
+// }
+// typedef struct
+// {
+//     int index1;
+//     int index2;
+// } Indices;
 
-Indices linearSearch(int arr[], int target)
-{
-    Indices result = {-1, -1};
-    int sz = sizeof(arr) / sizeof(arr[0]);
-    for (int i = 0; i < sz; i++)
-    {
-        for (int j = 0; j < i; j++)
-        {
-            if (arr[i] + arr[j] == target)
-            {
-                result.index1 = i;
-                result.index2 = j;
-                return result;
-            }
-        }
+// Indices linearSearch(int arr[], int target)
+// {
+//     Indices result = {-1, -1};
+//     int sz = sizeof(arr) / sizeof(arr[0]);
+//     for (int i = 0; i < sz; i++)
+//     {
+//         for (int j = 0; j < i; j++)
+//         {
+//             if (arr[i] + arr[j] == target)
+//             {
+//                 result.index1 = i;
+//                 result.index2 = j;
+//                 return result;
+//             }
+//         }
 
-        // if (arr[i] == target)
-        // {
-        //     return i;
-        // }
-    }
-    return result;
+//         // if (arr[i] == target)
+//         // {
+//         //     return i;
+//         // }
+//     }
+//     return result;
+// }
+
+void changeA(int &b) // Pass by reference using alias
+{
+    b = 20;
 }
 
 int main()
 {
+
+    int arr[] = {3, 2, 4};
+    int target = 6;
+
+    int a = 6;
+    int *ptr = &a;
+    int **parptr = &ptr;
+
+    changeA(a); // Pass by reference(alias), so it changes the value of a from 6 -> 20
+
+    cout << &a << endl;
+    cout << &ptr << endl;
+
+    cout << *(ptr) << endl;
+    cout << **(parptr) << endl;
+
+    cout << ptr << endl;
+    ptr++; // addr will added by 4 bytes means from 200 -> 204
+    cout << ptr << endl;
+
+    int arr1[] = {1, 2, 3, 4};
+
+    cout << *arr1 << endl;       // value of addr (0xdd38dff9a0)
+    cout << *(arr1 + 1) << endl; // value of addr (0xdd38dff9a0 + 4 = 0xebf53ffaa4)
+    cout << *(arr1 + 2) << endl;
+    cout << *(arr1 + 3) << endl;
+
     // int marks[] = {100, 24, 3, 44, 5};
     // int smallest = INT_MAX;
     // int biggest = INT_MIN;
@@ -62,11 +94,6 @@ int main()
     // }
     // printf("The Minimun is : %d \n", smallest);
     // printf("The Maximum is : %d \n", biggest);
-
-    int arr[] = {3, 2, 4};
-
-    int target = 6;
-
     // ChangeArr(arr, 4);
 
     // printf("in main\n");
@@ -75,7 +102,7 @@ int main()
     //     printf("%d ", arr[i]);
     // }
 
-    Indices val = linearSearch(arr, target);
-    printf("[%d,%d]", val.index2, val.index1);
+    // Indices val = linearSearch(arr, target);
+    // printf("[%d,%d]", val.index2, val.index1);
     return 0;
 }
