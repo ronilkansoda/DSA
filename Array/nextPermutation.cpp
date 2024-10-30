30872ea69cf3d0a384f0381b3560f339781d0c06
ronilkansoda <ronilkansoda73@gmail.com>
2024-10-30 13:02:20 +0530

    adding Strings Folder

--- Array/nextPermutation.cpp
+++ Array/nextPermutation.cpp
@@ -10,6 +10,7 @@ void nextPermutation(vector<int> &nums)
 {
     int pivot = -1, n = nums.size();
 
+    // finding pivot
     for (int i = n - 2; i >= 0; i--)
     {
         if (nums[i] < nums[i + 1])
@@ -19,12 +20,14 @@ void nextPermutation(vector<int> &nums)
         };
     }
 
+    // for decreasing order [3,2,1] so we reverse it to [1,2,3]
     if (pivot == -1)
     {
         reverse(nums.begin(), nums.end());
         return;
     }
 
+    // as it will take [1,2,5,4,3] and swap 3 and 2 as it accepts the if condition as 3 > 2
     for (int i = n - 1; i > pivot; i--)
     {
         if (nums[i] > nums[pivot])
@@ -34,8 +37,10 @@ void nextPermutation(vector<int> &nums)
         }
     }
 
+    // arr = [1,3,5,4,2] now i will be arr[i] = 5 and j will be arr[j] = 2
     int i = pivot + 1, j = n - 1;
 
+    // we will reverse from 5 to 2 and at last it will became arr [1,3,2,4,5]
     while (i <= j)
     {
         swap(nums[i++], nums[j--]);
