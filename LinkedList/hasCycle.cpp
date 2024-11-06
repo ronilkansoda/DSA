8bcb09887a7815400cbc458257539f036aaee37a
ronilkansoda <ronilkansoda73@gmail.com>
2024-11-06 20:14:47 +0530

    adding hasCycle.cpp

---
+++ LinkedList/hasCycle.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+
+struct Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int data1, Node *next1)
+    {
+        data = data1;
+        next = next1;
+    }
+
+    Node(int data1)
+    {
+        data = data1;
+        next = nullptr;
+    }
+};
+
+Node *convertArr2LL(vector<int> &arr)
+{
+    Node *head = new Node(arr[0]);
+    Node *mover = head;
+    for (int i = 1; i < arr.size(); i++)
+    {
+        Node *temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+
+    return head;
+}
+
+void printLL(Node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+bool hasCycle(Node *head)
+{
+    Node *slow = head;
+    Node *fast = head;
+
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+int main()
+{
+
+    vector<int> arr = {3};
+    Node *head = convertArr2LL(arr);
+    // head = hasCycle(head);
+
+    // printLL(head);
+    cout << hasCycle(head) << endl;
+
+    return 0;
+}
