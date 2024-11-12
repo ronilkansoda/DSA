36ca53cb15d740a7230a78b92a35da3949c88ec4
ronilkansoda <ronilkansoda73@gmail.com>
2024-11-12 22:55:30 +0530

    adding addTwoNumbers.cpp

---
+++ LinkedList/addTwoNumbers.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int data1, Node *next1)
+    {
+        data = data1;
+        next = next1;
+    }
+
+    Node(int data1)
+    {
+        data = data1;
+        next = nullptr;
+    }
+};
+
+Node *convertArr2LL(vector<int> &arr)
+{
+    Node *head = new Node(arr[0]);
+    Node *mover = head;
+    for (int i = 1; i < arr.size(); i++)
+    {
+        Node *temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+
+    return head;
+}
+void printLL(Node *head)
+{
+    Node *temp = head;
+    while (temp != nullptr)
+    {
+        // Print the data of the current node
+        cout << temp->data << " ";
+        // Move to the next node
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+Node *addTwoNumbers(Node *l1, Node *l2)
+{
+    Node *dummy = new Node();
+    Node *temp = dummy;
+    int carry = 0;
+    while ((l1 != NULL || l2 != NULL) || carry)
+    {
+        int sum = 0;
+        if (l1 != NULL)
+        {
+            sum += l1->data;
+            l1 = l1->next;
+        }
+
+        if (l2 != NULL)
+        {
+            sum += l2->data;
+            l2 = l2->next;
+        }
+
+        sum += carry;
+        carry = sum / 10;
+        Node *node = new Node(sum % 10);
+        temp->next = node;
+        temp = temp->next;
+    }
+    return dummy->next;
+}
+
+int main()
+{
+
+    vector<int> arr1 = {4, 1, 8, 4, 5};
+    vector<int> arr2 = {5, 6, 1, 8, 4, 5};
+    Node *head1 = convertArr2LL(arr1);
+    Node *head2 = convertArr2LL(arr2);
+    Node *intersection = addTwoNumbers(head1, head2);
+
+    printLL(intersection);
+    return 0;
+}
