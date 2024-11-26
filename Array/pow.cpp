1a618503acb83b3b7a99d7fd97335fec22811cd7
ronilkansoda <ronilkansoda73@gmail.com>
2024-11-26 15:21:27 +0530

    adding recursive approach

--- Array/pow.cpp
+++ Array/pow.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-double myPow(double x, int n)
+double myPow1(double x, int n)
 {
     if (n == 0)
         return 1.0;
@@ -37,10 +37,47 @@ double myPow(double x, int n)
     return ans;
 }
 
+double myPowRecursion(long binForm, double ans, double x)
+{
+    if (binForm % 2 == 1)
+    {
+        return myPowRecursion(binForm / 2, ans * x, x * x);
+    }
+    if (binForm == 0)
+    {
+        return ans;
+    }
+    return myPowRecursion(binForm /= 2, ans, x *= x);
+}
+
+double myPow(double x, int n)
+{
+    if (n == 0)
+        return 1.0;
+    if (x == 0)
+        return 0.0;
+    if (x == 1)
+        return 1.0;
+    if (x == -1 && n % 2 == 0)
+        return 1.0;
+    if (x == -1 && n % 2 != 0)
+        return -1.0;
+
+    long binForm = n;
+    if (n < 0)
+    {
+        x = 1 / x;
+        binForm = -binForm;
+    }
+
+    double ans = 1;
+    return myPowRecursion(binForm, ans, x);
+}
+
 int main()
 {
-    double x = 3.000;
-    int n = 5;
+    double x = 2.000;
+    int n = 10;
 
     double result = myPow(x, n);
 
