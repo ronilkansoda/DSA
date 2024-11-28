9c5b17762aa9b9b6e57ef0aebd778ec9c39235e5
ronilkansoda <ronilkansoda73@gmail.com>
2024-11-28 20:03:50 +0530

    adding generateParentheses.cpp

---
+++ Recursion/generateParentheses.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//                                   " "
+//                                    (
+//                         /--------  |  --------\
+//                        ((                     ()
+//                      /      \                   \
+//                    (((       (()                ()(
+//                  /         /     \            /     \
+//                ((()))   (()())   (())()     ()()()   ()(())
+
+void genPar(int open, int close, string current, vector<string> &result, int n)
+{
+    if (current.length() == 2 * n)
+    {
+        result.push_back(current);
+        return;
+    }
+    if (open < n)
+    {
+        genPar(open + 1, close, current + "(", result, n);
+    }
+    if (close < open)
+    {
+        genPar(open, close + 1, current + ")", result, n);
+    }
+}
+
+vector<string> generateParenthesis(int n)
+{
+    vector<string> result;
+    genPar(0, 0, "", result, n);
+    return result;
+}
+
+int main()
+{
+    vector<string> ans = generateParenthesis(3);
+
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << ",";
+    }
+
+    return 0;
+}
