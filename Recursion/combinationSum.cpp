4f45c00d79cc61acc16c1db341a324c2f2dd4121
ronilkansoda <ronilkansoda73@gmail.com>
2024-11-30 19:42:05 +0530

    help: adding comments

--- Recursion/combinationSum.cpp
+++ Recursion/combinationSum.cpp
@@ -6,20 +6,22 @@ void findCombination(int ind, int target, vector<int> &arr, vector<vector<int>> 
 {
     if (ind == arr.size())
     {
+        // if target difference decrease thai 0 thai jai to ds vector(array) ma je append kariyu che e return karisu as ans
         if (target == 0)
         {
             ans.push_back(ds);
         }
         return;
     }
-    // pick up the element
+
     if (arr[ind] <= target)
     {
         ds.push_back(arr[ind]);
+        // aa nichenu recursion if mare 2ej repeat karvu hoi ena mate
         findCombination(ind, target - arr[ind], arr, ans, ds);
         ds.pop_back();
     }
-
+    //  aa nichenu recursion if mara ds ma 2,2 che and target = 7-4 = 3 thayu to indx increase kari next number ma javamate
     findCombination(ind + 1, target, arr, ans, ds);
 }
 
